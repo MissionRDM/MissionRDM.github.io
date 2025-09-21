be303ac88faea5a0f63127c1a55ac3a611c2ed75
MissionRDM <jo.sommer1233@gmail.com>
2025-09-21 20:32:16 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -68,7 +68,7 @@ const routes = [
 // Create the router instance with the defined routes and history mode
 // Using createWebHashHistory for hash-based routing 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 

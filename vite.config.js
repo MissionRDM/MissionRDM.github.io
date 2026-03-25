9ab233cc63f38aec6d45a299ea5b4637481e07bf
Jodicooli <johanna.summermatter@outlook.com>
2026-03-25 12:48:23 +0100

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/MissionRDM/',
+  base: process.env.NODE_ENV === 'production' ? '/MissionRDM/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
